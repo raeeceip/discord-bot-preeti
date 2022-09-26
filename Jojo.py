ba3fe9ae181d7b4a69dca5774b0a01be6642de5c
Chiso <chiboguchisomu@gmail.com>
2022-09-27 00:02:27 +0100

    yes

--- Jojo.py
+++ Jojo.py
@@ -1,63 +1,125 @@
 import discord
+import os
+import pyjokes
 import random
+import requests
+import json
+from dotenv import load_dotenv
 import discord.ext
+from discord.ext import commands
+from discord.ext.commands import Bot
+
+# get token, server id from env
+load_dotenv(".env")
+token = os.getenv("TOKEN")
 
 
+client = discord.Client(intents=discord.Intents.all())
 
-client = discord.Client()
+Bot.command.prefix = "!"
+
+
+def get_quote():
+    response = requests.get("https://zenquotes.io/api/random")
+    json_data = json.loads(response.text)
+    quote = json_data[0]["q"] + " -" + json_data[0]["a"]
+    return quote
+
+
+def get_meme():
+    response = requests.get("https://meme-api.herokuapp.com/gimme")
+    json_data = json.loads(response.text)
+    meme = json_data["url"]
+    return meme
+
+
+def get_jokes():
+    joke = pyjokes.get_joke()
+    return joke
+
 
 @client.event
 async def on_ready():
-    print(' {0.user} is logged in'.format(client))
+    print(" {0.user} is logged in".format(client))
     print("bot online")
+    await client.change_presence(
+        activity=discord.Activity(type=discord.ActivityType.watching, name="you")
+    )
 
 
 @client.event
 async def on_message(message):
-    username = str(message.author).split('#')[0]
+    username = str(message.author).split("#")[0]
     user_message = str(message.content)
     channel = str(message.channel.name)
-    print(f'{username}: {user_message} {channel}')
+    print(f"{username}: {user_message} {channel}")
 
     if message.author == client.user:
         return
 
-    if message.channel.name == 'general':
-        if user_message.lower() == 'hey preeti':
-            await message.channel.send(
-                f'Hello {username} how can I help you today')
+    if message.channel.name == "general":
+        if user_message.lower() == "hey preeti":
+            await message.channel.send(f"Hello {username} how can I help you today")
             return
-        elif user_message.lower() == 'byee':
+        elif user_message.lower() == "byee":
             await message.channel.send("Bye! <3")
             return
-        elif user_message.lower() == 'how pretty am i today?':
+        elif user_message.lower() == "joke":
+            await message.channel.send(get_jokes())
+            return
+        elif user_message.lower() == "cow joke":
+            # fetch jokes from external file
+            jokes = open("jokes.txt", "r")
+            joke = random.choice(jokes.readlines())
+            await message.channel.send(joke)
+            return
+        elif user_message.lower() == "quote":
+            quote = get_quote()
+            await message.channel.send(quote)
+            return
+        elif user_message.lower() == "meme":
+            meme = get_meme()
+            await message.channel.send(meme)
+            return
+        elif user_message.lower() == "how pretty am i today?":
             responses = [
-                f'On a scale of 1-100, you are a {random.randrange(500,1000,400)}, shatter my scale kodi!',
+                f"On a scale of 1-100, you are a {random.randrange(500,1000,400)}, shatter my scale kodi!",
                 f'Based on my calculations, it looks like you"re a solid {random.randrange(500,1000,220)} today, amazing!',
                 f"Oh my! I've never measured a {random.randrange(10,1000,100)} on my metre before, this reading is off the charts!",
                 f"From what I see, I don't think i can measure it accurately, my apologies <3",
                 f"Unfortunately, I can't measure prettiness levels over {random.randrange(500,1000,5)}, make my machine no break abeg",
-                f"Oh sunshine, there's no one that can compare to how pretty you are today; keep shining! <3"
+                f"Oh sunshine, there's no one that can compare to how pretty you are today; keep shining! <3",
             ]
             await message.channel.send(responses[random.randrange(0, 5, 1)])
             return
-
-    if user_message.lower() == "tell me i'm pretty today":
-        await message.channel.send("You're so pretty Kodi")
-        return
+        elif user_message.lower() == "tell me i'm pretty today":
+            await message.channel.send("You're so pretty Kodi")
+            return
 
 
 @client.event
+async def on_member_join(member):
+    print(f"{member} has joined the server")
+    await member.send(f"Hi {member.name}, welcome to the server!")
+    await member.send("I'm Preeti, your personal assistant")
+    await member.send("How can I help you today?")
+    await member.send("Type 'help' to see what I can do for you")
+    return
+
+
+# on guild join
+@client.event
 async def on_guild_join(guild):
-    channel = 'general'
+    guild = guild
+    channel = "general"
     await channel.send(f"Hi there, my name's Preeti,wannatry me out?")
 
 
-@client.command()
+@Bot.command
 async def ping(ctx):
     await ctx.send(
         "pong!"
-    )  #simple command so that when you type "!ping" the bot will respond with "pong!"
+    )  # simple command so that when you type "!ping" the bot will respond with "pong!"
 
 
 async def kick(ctx, member: discord.Member):
@@ -65,11 +127,9 @@ async def kick(ctx, member: discord.Member):
         await member.kick(reason=None)
         await ctx.send(
             "kicked " + member.mention
-        )  #simple kick command to demonstrate how to get and use member mentions
+        )  # simple kick command to demonstrate how to get and use member mentions
     except:
         await ctx.send("bot does not have the kick members permission!")
 
 
-
-
-client.run(TOKEN)
+client.run(token)
---
+++ ReadME.md
@@ -0,0 +1,42 @@
+# Preeti #(Preeti#9792)
+
+[]: # (c) 2019-2020 by [Chibogu Chisom](https://github.com/raeeceip)
+
+- This is Preeti, a Discord bot written in Python using the discord.py library.
+- She was made to do a lot of things, but she's still a work in progress.
+- Shes currently in the process of being rebuilt, so some things may not work as intended.
+- She can be found on the [Discord Bots](https://discordbots.org/bot/966103472074281070) website.
+
+## Commands
+
+- Preeti was made to be a multipurpose bot designed to help cheer my best friend up on her birthday, so she has one main command, "How pretty am I today?" that returns a random compliment.
+
+### She has a few other commands, most of which are still being worked on.
+
+- `!help` - Shows this message.
+- 'joke' - Tells a joke
+- 'quote' - Sends an inspirational quote
+- 'meme' - Sends a meme
+- 'cow joke' - Sends a random cow joke from jokes.txt
+- She also has a few other commands on the way, along with some other helpful friends :), but they're still being worked on.
+
+## Contributing
+
+- If you'd like to contribute to this project, feel free to fork it and make a pull request.
+- If you'd like to add a feature, please make sure to make a new branch and make a pull request to the dev branch.
+- If you'd like to report a bug, please make an issue and I'll try to fix it as soon as possible.
+
+## License
+
+- This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
+
+## Acknowledgments
+
+- [Chibogu Chisom](https://raeeceip.github.io) - Creator of Preeti
+- [Discord.py](https://discordpy.readthedocs.io/en/stable/)
+- [pyjokes](https://pypi.org/project/pyjokes/) - Joke API
+- [zenquotes](https://zenquotes.io/api/random) - For the quotes
+- [meme-api](https://meme-api.herokuapp.com/gimme) - For the memes
+- [Discord Bots](https://discordbots.org/bot/966103472074281070) - Where you can find Preeti
+- [Discord](https://discord.com/) - For being a great platform for bots like Preeti
+- [Khadijat]() - For being my best friend and inspiration for Preeti
